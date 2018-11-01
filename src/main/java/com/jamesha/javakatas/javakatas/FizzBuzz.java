803ce7280d660f123054aef96665e934e449a650
James Ha <hajamesjw@gmail.com>
2018-11-01 09:20:29 -0400

    FizzBuzz: Formatting changes.

--- src/main/java/com/jamesha/javakatas/javakatas/FizzBuzz.java
+++ src/main/java/com/jamesha/javakatas/javakatas/FizzBuzz.java
@@ -12,10 +12,9 @@ public class FizzBuzz {
 	}
 	
 	public static String fizzBuzz(int x) {
-		
 		if (x < 1) {
 			throw new IllegalArgumentException();
-	    } else if (x % 3 == 0 && x % 5 == 0) {
+		} else if (x % 3 == 0 && x % 5 == 0) {
 			return "FizzBuzz";
 		} else if (x % 3 == 0) {
 			return "Fizz";
