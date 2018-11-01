b35a73c071cc83ee660bfc83132dc228b4953cb0
James Ha <hajamesjw@gmail.com>
2018-11-01 08:28:42 -0400

    First commit.

---
+++ src/main/java/com/jamesha/javakatas/javakatas/App.java
@@ -0,0 +1,13 @@
+package com.jamesha.javakatas.javakatas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
